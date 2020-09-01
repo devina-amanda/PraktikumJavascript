1f97afa78cde74fc5e7542d137564b0b5485bfa3
unknown <devina_amanda_28rpl@student.smktelkom-mlg.sch.id>
2020-09-01 14:25:38 +0700

    menambah revisi

--- 4.js
+++ 4.js
@@ -1,9 +1,11 @@
-let a = 4;
-let r = 3;
-
-for(i = 1; i <= 10; i++){
-
-   console.log(a)
-   a += 3
-}
-
+// mencari jumlah deret  
+let a = 4;  
+let hasil = 0;  
+  
+for(let i = 1;i<=10; i++){  
+  
+    console.log(a)  
+    hasil+=a  
+    a *= 3  
+}  
+console.log("Jumlah 10 suku pertama :" +hasil);
